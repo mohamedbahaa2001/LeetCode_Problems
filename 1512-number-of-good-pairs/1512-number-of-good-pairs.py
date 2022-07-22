cde59a6fa2e9750fbd4e0eadbd6597818049698d
Mohamed Bahaa <30205607+mohamedbahaa2001@users.noreply.github.com>
2022-07-22 19:18:55 +0200

    Time: 66 ms (18.47%), Space: 14 MB (10.41%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            for j in range(n):
+                if nums[i] == nums[j] and i < j:
+                    count += 1
+        return count
